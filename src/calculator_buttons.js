15e000b1f8ddc4a32918506d6c501489f083b552
zarif-al <zarif_al96@outlook.com>
2020-11-18 20:42:56 +0600

    first commit

---
+++ src/calculator_buttons.js
@@ -0,0 +1,107 @@
+//checkSign(button.value) ? pressedSignBtn : pressedNumBtn
+const buttonValues = [
+    {
+        class : "jumbo button",
+        id : "clear",
+        show : "AC",
+        value : "AC"
+    },
+    {
+        class : "button",
+        id : "divide",
+        show : "/",
+        value : "/"
+    },
+    {
+        class : "button",
+        id : "multiply",
+        show : "X",
+        value : "*"
+    },
+    {
+        class : "button",
+        id : "seven",
+        show : "7",
+        value : "7"
+    },
+    {
+        class : "button",
+        id : "eight",
+        show : "8",
+        value : "8"
+    },
+    {
+        class : "button",
+        id : "nine",
+        show : "9",
+        value : "9"
+    },
+    {
+        class : "button",
+        id : "subtract",
+        show : "-",
+        value : "-"
+    },
+    {
+        class : "button",
+        id : "four",
+        show : "4",
+        value : "4"
+    },
+    {
+        class : "button",
+        id : "five",
+        show : "5",
+        value : "5"
+    },
+    {
+        class : "button",
+        id : "six",
+        show : "6",
+        value : "6"
+    },
+    {
+        class : "button",
+        id : "add",
+        show : "+",
+        value : "+"
+    },
+    {
+        class : "button",
+        id : "one",
+        show : "1",
+        value : "1"
+    },
+    {
+        class : "button",
+        id : "two",
+        show : "2",
+        value : "2"
+    },
+    {
+        class : "button",
+        id : "three",
+        show : "3",
+        value : "3"
+    },
+    {   
+        class : "equal button",
+        id : "equals",
+        show : "=",
+        value : "="
+    },
+    {
+        class : "jumbo button",
+        id : "zero",
+        show : "0",
+        value : "0"
+    },
+    {
+        class : "button",
+        id : "decimal",
+        show : ".",
+        value : "."
+    }
+]
+
+export default buttonValues;
